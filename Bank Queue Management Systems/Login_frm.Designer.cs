573f239579b21be4303941e27fb5c2b2e8186284
SuonSopheak <Camboy0505@gmail.com>
2021-06-16 22:22:11 +0700

    Button ler login form

--- Bank Queue Management Systems/Login_frm.Designer.cs
+++ Bank Queue Management Systems/Login_frm.Designer.cs
@@ -36,6 +36,8 @@ namespace Bank_Queue_Management_Systems
             this.btnLogin = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.showPass = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtLoginName
@@ -102,11 +104,31 @@ namespace Bank_Queue_Management_Systems
             this.showPass.UseVisualStyleBackColor = true;
             this.showPass.CheckedChanged += new System.EventHandler(this.showPass_CheckedChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 64);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(212, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Login_frm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(534, 255);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.showPass);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnLogin);
@@ -133,5 +155,7 @@ namespace Bank_Queue_Management_Systems
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.CheckBox showPass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
